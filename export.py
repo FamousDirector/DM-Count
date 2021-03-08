f98ff27e9dcf428e0bcbbbdb057461c88bff8115
James Cameron <jamesc@patriot1tech.com>
2021-03-08 19:31:54 -0400

    make sure to set both outputs with dynamic dims

--- export.py
+++ export.py
@@ -29,7 +29,7 @@ torch.onnx.export(model,  # model being run
                   input_names=['input'],  # the model's input names
                   output_names=['output'],  # the model's output names
                   dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
-                                'output': {0: 'batch_size'}}
+                                'output': {0: 'batch_size'}, '105': {0: 'batch_size'}}
                   )
 
 onnx_model = onnx.load(output_model_filepath)
